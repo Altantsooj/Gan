dfd7f44b139c9b231e3b347ceac7d1fcbc468a3c
Alex Nicolaou <alex@stockgamblers.com>
2022-10-12 13:42:00 -0400

    Stickering cleaned up to just the string option

--- src/lib/components/Cube.svelte
+++ src/lib/components/Cube.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
 	import { CubieCube, Mask } from '$lib/third_party/onionhoney/CubeLib';
 	import type { MaskT } from '$lib/third_party/onionhoney/CubeLib';
-	import { Alg } from 'cubing/alg';
 	import { TwistyPlayer } from 'cubing/twisty';
 	import { onMount } from 'svelte';
 
@@ -12,7 +11,6 @@
 	export let playHead = 0;
 	export let stickering = '';
 	export let stickeringOrientation = '';
-	import { cube3x3x3 } from 'cubing/puzzles';
 
 	async function setStickersString(mask: MaskT) {
 		const cubies = new CubieCube().apply(stickeringOrientation);
@@ -63,142 +61,14 @@
 
 		twistyPlayer.experimentalStickeringMaskOrbits = `${edges},${corners},${centers}`;
 	}
-	async function setStickersKPuzzle(mask: MaskT) {
-		const regular = 'regular';
-		const muted = 'dim';
-		const UF = [regular, regular];
-		const UR = [regular, regular];
-		const UB = [regular, regular];
-		const UL = [regular, regular];
-		const DF = [regular, regular];
-		const DR = [regular, regular];
-		const DB = [regular, regular];
-		const DL = [regular, regular];
-		const FR = [regular, regular];
-		const FL = [regular, regular];
-		const BR = [regular, regular];
-		const BL = [regular, regular];
-		const edgeOrder = [UF, UL, UB, UR, DF, DL, DB, DR, FL, BL, BR, FR];
-		const UFR = [regular, regular, regular];
-		const URB = [regular, regular, regular];
-		const UBL = [regular, regular, regular];
-		const ULF = [regular, regular, regular];
-		const DRF = [regular, regular, regular];
-		const DFL = [regular, regular, regular];
-		const DLB = [regular, regular, regular];
-		const DBR = [regular, regular, regular];
-		const cornerOrder = [ULF, UBL, URB, UFR, DFL, DLB, DBR, DRF];
-		const U = [regular];
-		const L = [regular];
-		const F = [regular];
-		const R = [regular];
-		const B = [regular];
-		const D = [regular];
-		const centerOrder = [U, D, F, B, L, R];
-		const json = {
-			orbits: {
-				EDGES: {
-					pieces: [
-						{ facelets: UF },
-						{ facelets: UR },
-						{ facelets: UB },
-						{ facelets: UL },
-						{ facelets: DF },
-						{ facelets: DR },
-						{ facelets: DB },
-						{ facelets: DL },
-						{ facelets: FR },
-						{ facelets: FL },
-						{ facelets: BR },
-						{ facelets: BL }
-					]
-				},
-				CORNERS: {
-					pieces: [
-						{ facelets: UFR },
-						{ facelets: URB },
-						{ facelets: UBL },
-						{ facelets: ULF },
-						{ facelets: DRF },
-						{ facelets: DFL },
-						{ facelets: DLB },
-						{ facelets: DBR }
-					]
-				},
-				CENTERS: {
-					pieces: [
-						{ facelets: U },
-						{ facelets: L },
-						{ facelets: F },
-						{ facelets: R },
-						{ facelets: B },
-						{ facelets: D }
-					]
-				}
-			}
-		};
 
-		for (let i = 0; i < edgeOrder.length; ++i) {
-			if (mask.ep[i] === 1) {
-				edgeOrder[i][0] = regular;
-				edgeOrder[i][1] = regular;
-			} else {
-				edgeOrder[i][0] = muted;
-				edgeOrder[i][1] = muted;
-			}
-		}
-
-		for (let i = 0; i < cornerOrder.length; ++i) {
-			if (mask.cp[i] === 1) {
-				cornerOrder[i][0] = regular;
-				cornerOrder[i][1] = regular;
-				cornerOrder[i][2] = regular;
-			} else {
-				cornerOrder[i][0] = muted;
-				cornerOrder[i][1] = muted;
-				cornerOrder[i][2] = muted;
-			}
-		}
-
-		for (let i = 0; i < centerOrder.length; ++i) {
-			if (!mask.tp || mask.tp[i] === 1) {
-				centerOrder[i][0] = regular;
-			} else {
-				centerOrder[i][0] = muted;
-			}
-		}
-		function transformMask(mask, transformation) {
-			const newMask = { orbits: {} };
-			for (const [orbit, orbitMask] of Object.entries(mask.orbits)) {
-				const newOrbitMask = { pieces: [] };
-				const { numPieces, numOrientations } = transformation.kpuzzle.definition.orbits[orbit];
-				for (let i = 0; i < numPieces; i++) {
-					const perm = transformation.transformationData[orbit].permutation[i];
-					const ori = transformation.transformationData[orbit].orientation[i];
-					let facelets = orbitMask.pieces[perm].facelets.slice(0, numOrientations);
-					facelets = facelets.slice(ori).concat(facelets.slice(0, ori));
-					newOrbitMask.pieces.push({ facelets });
-				}
-				newMask.orbits[orbit] = newOrbitMask;
-			}
-			return newMask;
-		}
-		const kpuzzle = await cube3x3x3.kpuzzle();
-
-		twistyPlayer.experimentalStickeringMaskOrbits = transformMask(
-			json,
-			kpuzzle.algToTransformation(stickeringOrientation)
-		);
-	}
 	async function setStickers(mask: MaskT) {
-		setStickersKPuzzle(mask);
-		//setStickersString(mask);
+		setStickersString(mask);
 	}
 
 	$: if (stickering) {
 		console.log('RESET STICKERING to: ', stickering);
 		const stageToMask: { [key: string]: MaskT } = {};
-		// disable for now; rotations don't work.
 		stageToMask['fb'] = Mask.fb_mask;
 		stageToMask['ss'] = Mask.sb_mask;
 		stageToMask['sp'] = Mask.sb_mask;
@@ -240,7 +110,7 @@
 		let contentElem = document.querySelector('#twisty-content');
 		if (contentElem) {
 			twistyPlayer.background = 'none';
-			//twistyPlayer.visualization = 'PG3D';
+			twistyPlayer.visualization = 'PG3D';
 			if (controlPanel === 'none') {
 				twistyPlayer.controlPanel = 'none';
 			}
